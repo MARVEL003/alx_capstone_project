7d038242f529bb1b015309bae8f04bbb6d8e33b3
MARVEL <chimchetarammarvel@gmail.com>
2023-10-19 03:46:10 -0700

    Commit !

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+  navLinks.style.right = "0px";
+}
+function hideMenu() {
+  navLinks.style.right = "-700px";
+}
+
 // JavaScript for smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
+    const targetId = this.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
 
-        window.scrollTo({
-            behavior: 'smooth',
-            top: targetSection.offsetTop,
-        });
+    hideMenu()
+    window.scrollTo({
+      behavior: "smooth",
+      top: targetSection.offsetTop,
     });
+  });
 });
